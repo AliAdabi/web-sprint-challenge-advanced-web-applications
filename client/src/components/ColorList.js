4e00a940611c6ebe6f4679e9cad2e8d3c62bd1ab
Aliadabi <Aliadabi3780@gmail.com>
2020-07-18 10:55:49 -0700

    Not completely

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import {axiosWithAuth} from '../util/axiosWithAuth'
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ colors, updateColors, getColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -21,10 +21,29 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    let id = colorToEdit.id;
+    // console.log(id);
+    axiosWithAuth()
+     .put(`/api/colors/${id}`, colorToEdit)
+     .then(res => {
+      //  console.log(res);
+       updateColors(colors);
+       getColors();
+     })
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    // console.log(color);
+    let id = color.id;
+    // console.log(id);
+    axiosWithAuth()
+     .delete(`/api/colors/${id}`, colorToEdit)
+     .then(res => {
+       console.log(res);
+       updateColors(colors);
+       getColors();
+     })
   };
 
   return (
